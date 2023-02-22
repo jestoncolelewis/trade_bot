e69a954f62fc9b0bd2239f7080b213e67c0d8d84
Jeston Lewis <jeston@jestonlewiscreative.com>
2023-02-22 14:46:49 -0500

    combined account info and history get

--- paper_trade_bot.py
+++ paper_trade_bot.py
@@ -1,4 +1,5 @@
 from alpaca.trading.client import TradingClient
+from history import get_history
 
 API_KEY = "PKEBHMRF81CE33KXD7Y8"
 SECRET_KEY = "wxjQI2tbgnfRd0NNaVJpbbHJBKsW0sG4XOJzjDMd"
@@ -6,8 +7,8 @@ SECRET_KEY = "wxjQI2tbgnfRd0NNaVJpbbHJBKsW0sG4XOJzjDMd"
 trading_client = TradingClient(API_KEY, SECRET_KEY, paper=True)
 
 account = trading_client.get_account()
-for property_name, value in account:
-    print(f'"{property_name}": {value}')
+""" for property_name, value in account:
+    print(f'"{property_name}": {value}') """
 
 # Check if our account is restricted from trading.
 if account.trading_blocked:
@@ -15,3 +16,4 @@ if account.trading_blocked:
 
 # Check how much money we can use to open new positions.
 print('${} is available as buying power.'.format(account.buying_power))
+get_history('AAPL', 'GOOGL')
