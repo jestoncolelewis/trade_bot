bed46bfc701b0ee8990f596cd16cdf9a10f1be4e
Jeston Lewis <jeston@jestonlewiscreative.com>
2023-02-25 06:58:05 -0500

    changed key method and function calls

--- paper_trade_bot.py
+++ paper_trade_bot.py
@@ -1,14 +1,15 @@
 from alpaca.trading.client import TradingClient
-from history import get_history
+import functions as func
+from keys import *
 
-API_KEY = "PKEBHMRF81CE33KXD7Y8"
-SECRET_KEY = "wxjQI2tbgnfRd0NNaVJpbbHJBKsW0sG4XOJzjDMd"
+API_KEY = paper_ak
+SECRET_KEY = paper_sk
 
 trading_client = TradingClient(API_KEY, SECRET_KEY, paper=True)
 
 account = trading_client.get_account()
-""" for property_name, value in account:
-    print(f'"{property_name}": {value}') """
+for property_name, value in account:
+    print(f'"{property_name}": {value}')
 
 # Check if our account is restricted from trading.
 if account.trading_blocked:
@@ -16,4 +17,5 @@ if account.trading_blocked:
 
 # Check how much money we can use to open new positions.
 print('${} is available as buying power.'.format(account.buying_power))
-get_history('AAPL', 'GOOGL')
+func.get_history(API_KEY, SECRET_KEY, 'AAPL', 'GOOGL')
+func.buy_market(API_KEY, SECRET_KEY, 'AAPL', 'GOOGL')
