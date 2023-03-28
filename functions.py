d76a798283d84d002d3c34b6a64c07a4386e9af6
Jeston Lewis <jeston.lewis@protonmail.com>
2023-03-28 13:39:37 -0600

    fixed empty value issue

--- functions.py
+++ functions.py
@@ -4,22 +4,38 @@ from alpaca.trading.requests import MarketOrderRequest
 from alpaca.trading.enums import OrderSide, TimeInForce
 
 # multi symbol request - single symbol is similar
-def get_history(ak, sk, *args):
+def get_history(ak, sk, symbols):
     client = StockHistoricalDataClient(ak,  sk)
-    multisymbol_request_params = StockLatestQuoteRequest(symbol_or_symbols=[*args])
+    multisymbol_request_params = StockLatestQuoteRequest(symbol_or_symbols=symbols) # type: ignore
 
     latest_multisymbol_quotes = client.get_stock_latest_quote(multisymbol_request_params)
 
-    for arg in args:
-        latest_ask_price = latest_multisymbol_quotes[arg].ask_price
-        print('{} - ${}' .format(arg, latest_ask_price))
+    latest_prices = {}
+    for symbol in symbols:
+        latest_ask_price = latest_multisymbol_quotes[symbol].ask_price
+        latest_ask_price = '{}'.format(latest_ask_price)
+        latest_prices[symbol] = latest_ask_price
+    
+    return latest_prices
 
-def buy_market(ak, sk, *args):
-    for arg in args:
+def buy_market(ak, sk, symbols, prices):
+    trading_client = TradingClient(ak, sk, paper=True)
+
+    account = trading_client.get_account()
+
+    # Check if our account is restricted from trading.
+    if account.trading_blocked: # type: ignore
+        print('Account is currently restricted from trading.')
+    # Check how much money we can use to open new positions.
+    print('${} is available as buying power.'.format(account.buying_power)) # type:ignore
+
+    # divide buying power by number of symbols to produce purchase price
+    
+    for sym in symbols:
         trading_client = TradingClient(ak, sk, paper=True)
         market_order_data = MarketOrderRequest(
-                        symbol=arg,
-                        qty=0.023,
+                        symbol=sym,
+                        notional=prices[sym],
                         side=OrderSide.BUY,
                         time_in_force=TimeInForce.DAY
                         )
