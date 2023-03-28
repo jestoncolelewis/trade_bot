a90052bc16bf89d812d2b1f2e48f2402a8123caf
Jeston Lewis <jeston.lewis@protonmail.com>
2023-03-28 13:42:28 -0600

    added pandas, simplified to mainly function calls, added files reading for symbol extraction

--- paper_trade_bot.py
+++ paper_trade_bot.py
@@ -1,21 +1,15 @@
 from alpaca.trading.client import TradingClient
+import pandas
 import functions as func
 from keys import *
 
+# Key handling
 API_KEY = paper_ak
 SECRET_KEY = paper_sk
 
-trading_client = TradingClient(API_KEY, SECRET_KEY, paper=True)
+# Symbols
+data = pandas.read_csv('./constituents.csv')
+symbols = data['Symbol'].tolist()
 
-account = trading_client.get_account()
-for property_name, value in account:
-    print(f'"{property_name}": {value}')
-
-# Check if our account is restricted from trading.
-if account.trading_blocked:
-    print('Account is currently restricted from trading.')
-
-# Check how much money we can use to open new positions.
-print('${} is available as buying power.'.format(account.buying_power))
-func.get_history(API_KEY, SECRET_KEY, 'AAPL', 'GOOGL')
-func.buy_market(API_KEY, SECRET_KEY, 'AAPL', 'GOOGL')
+prices = func.get_history(API_KEY, SECRET_KEY, symbols)
+func.buy_market(API_KEY, SECRET_KEY, symbols, prices)
