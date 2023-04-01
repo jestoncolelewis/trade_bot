ce93ad90528bb7aefebb0cb5c030621b4f713e31
Jeston Lewis <jeston.lewis@protonmail.com>
2023-03-31 20:51:06 -0600

    laid out basic combiner

--- conform.py
+++ conform.py
@@ -1,1 +1,19 @@
 from data_in import *
+
+def combine(src1, src2):
+    data = []
+    for i in src1:
+        for j in src2:
+            if i['symbol'] != j['symbol']:
+                data.append(i)
+            else:
+                data.append(j)
+    return data
+
+r_data = reddit()
+
+t_data = twitter()
+
+data = combine(r_data, t_data)
+
+history = alpha(data)
